11a40a35ed02044a2fca2e696e423b22f7682d14
Leandro Sousa <leandro_rocker@yahoo.com.br>
2021-08-14 00:45:14 -0300

    fully functional pollutantmean function

--- pollutantmean.R
+++ pollutantmean.R
@@ -16,13 +16,14 @@ pollutantmean <- function( directory = "specdata", pollutant = "nitrate", id = 1
     }else{ 
       for( i in id){
         
-        if (id[i] >= 1 && id[i] <= 332 && counter == 0){
-          df <- read.csv(paste(directory,"/",sprintf( "%03d",id[i] ),".csv", sep =""))
-          counter <- counter + 1
-          
-        }else if(id[i] >= 1 && id[i] <= 332 && counter > 0) {
-          tempdf <- read.csv(paste(directory,"/",sprintf( "%03d",id[i] ),".csv", sep =""))
+        if (id[i] >= 1 && id[i] <= 332){
+          tempdf <- read.csv(paste0(directory,"/",sprintf( "%03d",id[i] ),".csv"))
           df <- rbind(df, tempdf)
+          #counter <- counter + 1
+          
+        #}else if(id[i] >= 1 && id[i] <= 332 && counter > 0){
+          #tempdf <- read.csv(paste0(directory,"/",sprintf( "%03d",id[i] ),".csv"))
+          #df <- rbind(df, tempdf)
           
         }else{
           print(paste( id[i], "is not a valid id, The id must be between 1 and 332!"))
